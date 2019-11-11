cd645d219dc24eac98a8aa2220ed586b879323dc
p.kotelnikova <p.kotelnikova@i-teco.ru>
2019-11-11 15:56:23 +0300

    Initial commit

---
+++ src/main/java/com/polinkot/demo3/Application.java
@@ -0,0 +1,14 @@
+package com.polinkot.demo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Polina Kotelnikova
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
